becfa87e4ec128438c1a1171f85d8075a441a0cb
n8upham <nsupham@uchicago.edu>
2021-05-04 22:52:15 -0700

    add BAMM results

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MamDiv-fossil-vs-timetree
-Data and code underlying Upham et al. 2021 in Current Biology
+Data and code underlying Upham et al. 2021 in review in _Current Biology_
---
+++ _RESULTS_output/BAMM_results/grace_2.5_1week_NDexp_9/bamm_on_Grace_week_9.sh
@@ -0,0 +1,10 @@
+#BSUB -J BAMM_100Mgens_1mamPhy
+#BSUB -n 10
+#BSUB -R "span[]"  
+#BSUB -q week
+#BSUB -W 168:00
+#BSUB -o outputs/BAMM_10Mgens_1mamPhy
+#BSUB -N
+
+echo "Slot count: $LSB_DJOB_NUMPROC"
+/apps/hpc/Apps/BAMM/2.5.0/bin/bamm -c mamPhy_controlFile_9.txt
